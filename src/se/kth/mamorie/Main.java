a4b5a7ca538e665679b1acf9da21667f7fcec6f3
Marie Kindblom <tuffamarie@hotmail.com>
2014-05-08 13:20:19 +0200

    Stänga fönster

--- src/se/kth/mamorie/Main.java
+++ src/se/kth/mamorie/Main.java
@@ -2,11 +2,44 @@ package se.kth.mamorie;
 
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Frame frame = new Frame("Memory");
+		
+		frame.addWindowListener(new WindowListener() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				System.exit(0);
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {				
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+			}
+		});
 		
 		FlowLayout layout = new FlowLayout();
 		layout.setHgap(Card.CARD_WIDTH/10);
